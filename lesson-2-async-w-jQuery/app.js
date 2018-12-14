060a72889be3ac2b777c1fbcdedad12ab7ec76fe
Zakaria Khan <zakaria_yahhia@hotmail.com>
2018-12-14 23:13:26 +0000

    Update app.js
    This is ajax requestion to unsplash.com and nyt for articles. 
    
    Thanks to Udacity, I have total control of ajax now :)

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,5 +1,7 @@
 /* eslint-env jquery */
 
+/* eslint-env jquery */
+
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
@@ -10,5 +12,46 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+// add image function which gets rendered in html page once returned succesfull
+      function addImage(thiscanbenamedanything) {
+    const firstImage = thiscanbenamedanything.results[0];
+    console.log(firstImage);
+    responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`
+    );
+}
+
+function addArticles(articles) {
+
+            const data = articles;
+            console.log(data);
+
+               responseContainer.insertAdjacentHTML('beforeend', '<ul>' + data.response.docs.map(article =>
+                    `<li class="article">
+                     <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                     <p>${article.snippet}</p>
+                     </li>`
+                     ).join('') + '</ul>');
+        }
+
+// makeing jquery ajax request
+        $.ajax({
+        	url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        	headers:
+        	{'Authorization': 'Client-ID 2f8dd55983a8084e313a47fbbdee8bb04204c717818973b423e09ac7786083b4'
+        }
+        }).done(addImage);
+
+        // Article ajax call
+
+        $.ajax({
+        	url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=88a17275c8a0462d8bc49274c6063134`,
+        }).done(addArticles)
+
+
+
     });
 })();
